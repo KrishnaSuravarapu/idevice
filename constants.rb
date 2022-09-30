4c7c9c4420ae785d556e8ef35b0f9bb10cfd6ff9
KrishnaSuravarapu <krishna.su@browserstack.com>
2022-09-30 20:31:23 +0530

    Task: Moving function declaration before calling

--- constants.rb
+++ constants.rb
@@ -1,6 +1,3 @@
-LIBIMOBILEDYLIB = get_libimobile_dylib.freeze
-LIBPLISTDYLIB = get_libplist_dylib.freeze
-
 def get_libimobile_dylib
   puts "Hey Harshit"
   `otool -L /usr/local/bin/idevice_id | grep -m 1 'libimobiledevice'`.strip().split[0]
@@ -10,3 +7,6 @@ def get_libplist_dylib
   puts "Hey Krishna"
   `otool -L /usr/local/bin/idevice_id | grep -m 1 'libplist'`.strip().split[0]
 end
+
+LIBIMOBILEDYLIB = get_libimobile_dylib.freeze
+LIBPLISTDYLIB = get_libplist_dylib.freeze
