0db136a4c61678a395fc160afe23515ad939fa15
KrishnaSuravarapu <krishna.su@browserstack.com>
2022-09-30 20:07:09 +0530

    Task: removing added dylib files and using otool to fetch dynamically

--- constants.rb
+++ constants.rb
@@ -1,2 +1,12 @@
-LIBIMOBILEDYLIB = "#{__dir__}/dependencies/libimobiledevice.dylib"
-LIBPLISTDYLIB = "#{__dir__}/dependencies/libplistnew.dylib"
+LIBIMOBILEDYLIB = get_libimobile_dylib.freeze
+LIBPLISTDYLIB = get_libplist_dylib.freeze
+
+def get_libimobile_dylib
+  puts "Hey Harshit"
+  `otool -L /usr/local/bin/idevice_id | grep -m 1 'libimobiledevice'`.strip().split[0]
+end
+
+def get_libplist_dylib
+  puts "Hey Krishna"
+  `otool -L /usr/local/bin/idevice_id | grep -m 1 'libplist'`.strip().split[0]
+end
