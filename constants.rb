ac64b9ee9843bcd33a4a34f86542ac016ba2c8ed
KrishnaSuravarapu <krishna.su@browserstack.com>
2022-10-03 14:02:37 +0530

    Task: Changing Function names

--- constants.rb
+++ constants.rb
@@ -1,8 +1,8 @@
-def get_libimobile_dylib
+def get_libplist_dylib
   `otool -L /usr/local/bin/idevice_id | grep -m 1 'libplist.*dylib' | awk '{print $1}'`
 end
 
-def get_libplist_dylib
+def get_libimobile_dylib
   `otool -L /usr/local/bin/idevice_id | grep -m 1 'libimobiledevice.*dylib' | awk '{print $1}'`
 end
 
